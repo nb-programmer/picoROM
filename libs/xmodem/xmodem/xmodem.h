6d86b81b45e83ca604d0460aff8201f58470533e
Narayan Bandodker <narayanband1356@gmail.com>
2025-05-20 09:41:38 +0530

    Fixed minor tab-indent issue

--- libs/xmodem/xmodem/xmodem.h
+++ libs/xmodem/xmodem/xmodem.h
@@ -11,44 +11,37 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
-
 /* Protocol variants */
-typedef enum
-{
-	XMODEM_MODE_ORIGINAL,
-	XMODEM_MODE_CRC
+typedef enum {
+    XMODEM_MODE_ORIGINAL,
+    XMODEM_MODE_CRC
 } xmodem_mode_t;
 
-
 /* Configuration parameters */
-typedef struct
-{
-	/* Enable/disable logging during the transfer.
-	 *
-	 * 0 = fatal errors only
-	 * 1 = errors only
-	 * 2 = verbose logging
-	 * 3 = log all bytes received
-	 */
-	int logLevel;
+typedef struct {
+    /* Enable/disable logging during the transfer.
+     *
+     * 0 = fatal errors only
+     * 1 = errors only
+     * 2 = verbose logging
+     * 3 = log all bytes received
+     */
+    int logLevel;
 
-	/* Enable the CRC variant of the protocol */
-	bool useCrc;
+    /* Enable the CRC variant of the protocol */
+    bool useCrc;
 
-	/* Enable decoding of escape characters */
-	bool useEscape;
+    /* Enable decoding of escape characters */
+    bool useEscape;
 } xmodem_config_t;
-
 
 /* Currently active configuration */
 extern xmodem_config_t xmodem_config;
 
-
 /* Change configuration */
 void xmodem_set_config(xmodem_mode_t mode);
 
-
-/* Receive a file.  Note that XMODEM pads files to the next multiple of 128, 
+/* Receive a file.  Note that XMODEM pads files to the next multiple of 128,
  * using character 26.
  *
  * Returns the amount of data received - always a multiple of 128.
@@ -56,17 +49,18 @@ void xmodem_set_config(xmodem_mode_t mode);
  *    outputBuffer: where to store the received data
  *    bufferSize:   maximum number of bytes to store in outputBuffer
  *    message:      an optional prompt to start the transfer
- *    inputhandler: an optional function to handle unexpected input before the transfer
+ *    inputhandler: an optional function to handle unexpected input before the
+ * transfer
  *
- * If message is provided, it will be printed every few seconds until the 
+ * If message is provided, it will be printed every few seconds until the
  * first data is received.
  *
- * If inputhandler is provided, it will be called if unexpected input is received before
- * the transfer begins.  The unexpected input character will be passed to the handler,
- * which should return 'true' if it handled it.
+ * If inputhandler is provided, it will be called if unexpected input is
+ * received before the transfer begins.  The unexpected input character will be
+ * passed to the handler, which should return 'true' if it handled it.
  */
-int xmodem_receive(void* outputBuffer, size_t bufferSize, const char *message, bool (*inputhandler)(int));
-
+int xmodem_receive(void *outputBuffer, size_t bufferSize, const char *message,
+                   bool (*inputhandler)(int));
 
 /* Dumps cached log data to stdout. */
 void xmodem_dumplog();
